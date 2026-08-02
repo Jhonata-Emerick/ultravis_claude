c02f9e4258eaa43a61e0f44b671d53b3653f779a
Jhon <jhon@comeca.ai>
2026-08-02 18:12:09 -0300

    Implement locale setting in layout component
    Added setRequestLocale function to set the locale for the request.

--- web/src/app/[locale]/layout.tsx
+++ web/src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
-import { getMessages } from 'next-intl/server';
+import { getMessages, setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { ThemeProvider } from 'next-themes';
 import { routing } from '@/i18n/routing';
@@ -17,6 +17,8 @@ export default async function LocaleLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
+  setRequestLocale(locale);
 
   const messages = await getMessages();
 
